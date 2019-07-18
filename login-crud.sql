4e95e7bc768a95f01632f329eb6294b65eeb0106
Amin Tahseen <51921201+AminTahseen@users.noreply.github.com>
2019-07-18 16:43:59 +0500

    Add files via upload

---
+++ login-crud.sql
@@ -0,0 +1,14 @@
+create database sts;
+use sts;
+show tables;
+
+create table tbl_user(uid int primary key auto_increment,username varchar(50),pass varchar(500));
+insert into tbl_user values(2,"user2","$2a$10$uyvxpAlgfTr3DXribfb39OMnEjW1ogfLz2hYa9s./o161mo7wdu7a"); #pass:User123
+insert into tbl_user values(3,"user3","$2a$10$LtO9grQ4w9iJ6KU1uIe1y.EELYB0wgjhXnMKZZraNkNhJuJi464la"); #pass:user3
+select * from tbl_user;
+desc tbl_user;
+
+create table tbl_employee(eid int primary key auto_increment,ename varchar(100),edesign varchar(100));
+insert into tbl_employee values(1,"Emp-1","Des-1");
+select * from tbl_employee;
+desc tbl_employee;
